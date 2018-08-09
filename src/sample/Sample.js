724be00a302cac4b26a55994639eedbc1cdce7f5
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2018-08-09 18:30:39 +0300

    Some initial pre-configurations

---
+++ src/sample/Sample.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export class Sample extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello, world!</h2>
+            </div>
+        )
+    }
+}
