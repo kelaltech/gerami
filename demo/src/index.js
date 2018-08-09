232ea19b870c004b3aea3fe9440fcee603f466d3
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2018-08-09 18:30:39 +0300

    Some initial pre-configurations

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import {Sample} from '../../src'
 
 class Demo extends Component {
   render() {
     return <div>
       <h1>gerami Demo</h1>
-      <Example/>
+      <Sample/>
     </div>
   }
 }
