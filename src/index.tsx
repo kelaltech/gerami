60627c356fd5c3631e3047ebf7d1179bbf1db30d
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2018-08-17 18:37:07 +0300

    bug fix for local development using yarn/npm link

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,16 @@
-// @ts-ignore
-import React from 'react'
-
-import { AccountPic } from './components/account-pic/AccountPic'
-import { Anchor } from './components/anchor/Anchor'
-import { Block } from './components/block/Block'
-import { Button } from './components/button/Button'
-import { Card } from './components/card/Card'
-import { Content } from './components/content/Content'
-import { FlexSpacer } from './components/flex-spacer/FlexSpacer'
-import { Image } from './components/image/Image'
-import { Input } from './components/input/Input'
-import { LeftDrawer } from './components/left-drawer/LeftDrawer'
-import { Loading } from './components/loading/Loading'
-import { MenuDrop } from './components/menu-drop/MenuDrop'
-import { MenuItem } from './components/menu-item/MenuItem'
-import { Page } from './components/page/Page'
-import { TextArea } from './components/text-area/TextArea'
-import { Warning } from './components/warning/Warning'
-
-export {
-  AccountPic,
-  Anchor,
-  Block,
-  Button,
-  Card,
-  Content,
-  FlexSpacer,
-  Image,
-  Input,
-  LeftDrawer,
-  Loading,
-  MenuDrop,
-  MenuItem,
-  Page,
-  TextArea,
-  Warning
-}
+export { AccountPic } from './components/account-pic/AccountPic.js'
+export { Anchor } from './components/anchor/Anchor.js'
+export { Block } from './components/block/Block.js'
+export { Button } from './components/button/Button.js'
+export { Card } from './components/card/Card.js'
+export { Content } from './components/content/Content.js'
+export { FlexSpacer } from './components/flex-spacer/FlexSpacer.js'
+export { Image } from './components/image/Image.js'
+export { Input } from './components/input/Input.js'
+export { LeftDrawer } from './components/left-drawer/LeftDrawer.js'
+export { Loading } from './components/loading/Loading.js'
+export { MenuDrop } from './components/menu-drop/MenuDrop.js'
+export { MenuItem } from './components/menu-item/MenuItem.js'
+export { Page } from './components/page/Page.js'
+export { TextArea } from './components/text-area/TextArea.js'
+export { Warning } from './components/warning/Warning.js'
