29bf5381bd5cabc6af764b902a51fe122db59b0e
Anteneh Ashenafi <anteneh845@gmail.com>
2018-08-21 09:45:22 +0300

    added dialog

---
+++ src/components/dialog/Dialog.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import './Dialog.scss'
+export declare class Dialog extends React.Component<any, any> {
+  constructor(props: any)
+  render(): JSX.Element
+}
---
+++ src/components/dialog/Dialog.js
@@ -0,0 +1,50 @@
+'use strict'
+var __extends =
+  (this && this.__extends) ||
+  (function() {
+    var extendStatics = function(d, b) {
+      extendStatics =
+        Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array &&
+          function(d, b) {
+            d.__proto__ = b
+          }) ||
+        function(d, b) {
+          for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]
+        }
+      return extendStatics(d, b)
+    }
+    return function(d, b) {
+      extendStatics(d, b)
+      function __() {
+        this.constructor = d
+      }
+      d.prototype = b === null ? Object.create(b) : ((__.prototype = b.prototype), new __())
+    }
+  })()
+Object.defineProperty(exports, '__esModule', { value: true })
+var React = require('react')
+require('./Dialog.scss')
+var Dialog = /** @class */ (function(_super) {
+  __extends(Dialog, _super)
+  function Dialog(props) {
+    var _this = _super.call(this, props) || this
+    _this.state = {
+      open: false
+    }
+    return _this
+  }
+  Dialog.prototype.render = function() {
+    return React.createElement(
+      'div',
+      { className: 'modal center', style: { display: this.props.open ? 'block' : 'none' } },
+      React.createElement(
+        'div',
+        { className: 'modal-content', style: { width: this.props.width } },
+        this.props.children
+      )
+    )
+  }
+  return Dialog
+})(React.Component)
+exports.Dialog = Dialog
